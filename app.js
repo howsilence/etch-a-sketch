5428f9e335431c5fd291a43618b731d9e0e70409
howsilence <86009962+howsilence@users.noreply.github.com>
2021-11-16 11:17:51 -0500

    11/16/21 semi-final build
    shader button still needs work
    maybe touch up ui a bit

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 // const declarations
 const container = document.getElementById('grid');
-const cells = document.getElementsByClassName('cell');
 const sml = document.getElementById('small-btn');
 const med = document.getElementById('medium-btn');
 const lrg = document.getElementById('large-btn');
 const eraseBtn = document.getElementById('erase-btn');
 const rows = document.getElementsByClassName('rcell');
 const f2bBtn = document.getElementById('f2b-btn');
+const colorBtn = document.getElementById('color-btn');
+const grayBtn = document.getElementById('gray-btn');
+
+
+
 
 // row and col functions
 const makeRows = (height) => {
     for (r = 0; r < height; r++) {
-        let rows = document.createElement('div');
-        rows.addEventListener('mouseenter', (e)=>{
-            event.target.setAttribute('id', 'moused');
-        });
+        let rows = document.createElement('div');     
         container.appendChild(rows).className = 'rcell';
         
     };
@@ -24,13 +25,10 @@ const makeColumns = (width) => {
      for(r = 0; r < rows.length; r++) {
         for (c = 0; c < width; c++) {
             let columns = document.createElement('div');
-            columns.addEventListener('mouseenter', (e)=>{
-                event.target.setAttribute('id', 'moused');
-            });
             container.appendChild(columns).className = "ccell";
+            };
         };
-   };
-};
+    };
 
 
 // create grid functions
@@ -42,6 +40,7 @@ const setupGridSmall = () => {
     makeColumns(width);
     container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
+    pointerStyle();
 };
 const setupGridMedium = () => {
     //grid medium = 32 * 52
@@ -51,6 +50,7 @@ const setupGridMedium = () => {
     makeColumns(width);
     container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
+    pointerStyle();
 };
 
 const setupGridLarge = () => {
@@ -61,11 +61,14 @@ const setupGridLarge = () => {
     makeColumns(width);
     container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
+    pointerStyle();
 };
 
+// default load function
+window.onload = setupGridSmall();
 
 
-// event listeners
+// button event listeners
 sml.addEventListener('click', ()=>{
     const gridArray = Array.from(container.childNodes);
             gridArray.forEach((element) => {
@@ -88,18 +91,83 @@ lrg.addEventListener('click', ()=>{
     setupGridLarge();
 });
 eraseBtn.addEventListener('click', ()=>{
+    f2bBtn.disabled = false;
+    colorBtn.disabled = false;
     const gridArray = Array.from(container.childNodes);
             gridArray.forEach((element) => {
                 container.removeChild(element);
             });
-})
-f2bBtn.addEventListener('click', ()=>{
-    const gridArray = Array.from(container.childNodes);
-    gridArray.forEach(fade());
+    setupGridSmall();
+    pointerStyle();
 });
-// misc function(s)
-function fade(){
-    let currentOpacity = +this.style.opacity;
-    if (currentOpacity < 1) currentOpacity += 0.1;
-  this.style.opacity = currentOpacity;
+f2bBtn.addEventListener('click',  e => {
+        f2bBtn.disabled = true;
+        colorBtn.disabled = false;
+        grayBtn.disabled = false;
+        pointerStyle();
+});
+colorBtn.addEventListener('click', e => {
+    f2bBtn.disabled = false;
+    colorBtn.disabled = true;
+    grayBtn.disabled = false;
+    pointerStyle();
+});
+grayBtn.addEventListener('click', e =>{
+    f2bBtn.disabled = false;
+    colorBtn.disabled = false;
+    grayBtn.disabled = true;
+    pointerStyle();
+});
+// hoisted functions
+function pointerStyle(){
+    if (f2bBtn.disabled == true){
+        console.log('fader active!');
+        const gridArray = Array.from(container.childNodes);
+            gridArray.forEach((element) => {
+                element.removeEventListener('mouseenter', techniColor);
+                element.removeEventListener('mouseenter', normalColor);
+                element.addEventListener('mouseenter', fader);
+            });
+    } else if (colorBtn.disabled == true){
+        console.log('color active!');
+        const gridArray = Array.from(container.childNodes);
+            gridArray.forEach((element) => {
+                element.removeEventListener('mouseenter', fader);
+                element.removeEventListener('mouseenter', normalColor);
+                element.addEventListener('mouseenter', techniColor);
+            });
+    } else if (grayBtn.disabled == true) {
+        console.log('default active!');
+        const gridArray = Array.from(container.childNodes);
+            gridArray.forEach((element) => {
+                element.removeEventListener('mouseenter', fader);
+                element.removeEventListener('mouseenter', normalColor);
+                element.addEventListener('mouseenter', normalColor);
+            });
+    } else {
+        console.log('default active!');
+        const gridArray = Array.from(container.childNodes);
+            gridArray.forEach((element) => {
+                element.removeEventListener('mouseenter', fader);
+                element.removeEventListener('mouseenter', normalColor);
+                element.addEventListener('mouseenter', normalColor);
+    });
+}};
+function randomColorGen(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
 };
+function normalColor(e){
+    e.target.style.backgroundColor = "rgb(122, 122, 122)";
+    e.target.style.opacity = 1;
+};
+function techniColor(e){
+    e.target.style.backgroundColor = '#' + randomColorGen();
+    e.target.style.opacity = 1;
+};
+function fader(e){
+    e.target.style.backgroundColor = "rgb(122, 122, 122)";
+    e.target.style.opacity = 0.2;
+    e.target.count += 1;
+    e.target.style.opacity = 0.2 * e.target.count;
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
 10/28 started etch-a-sketch for odin project. take two, need to refresh and rebuild from the ground up.
-11/1 finished basic etch-a-sketch. random color button and fade to black button still need work.
